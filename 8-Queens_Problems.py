39886243743d0a01331d36e995a576c523b015d7
Anatol <seddboom@gmail.com>
2021-03-01 20:43:03 +0300

    add 21 new files

---
+++ 8-Queens_Problems.py
@@ -0,0 +1,32 @@
+BOARD_SIZE = 8
+
+
+class BailOut(Exception):
+    pass
+
+
+def validate(list_queens):
+    left = right = col = list_queens[-1]
+    for r in reversed(list_queens[:-1]):
+        left, right = left-1, right+1
+        if r in (left, col, right):
+            raise BailOut
+
+
+def add_queen(list_queens):
+    for i in range(BOARD_SIZE):
+        test_queens = list_queens + [i]
+        try:
+            validate(test_queens)
+            if len(test_queens) == BOARD_SIZE:
+                return test_queens
+            else:
+                return add_queen(test_queens)
+        except BailOut:
+            pass
+    raise BailOut
+
+
+queens = add_queen([])
+print(queens)
+print("\n".join(". "*q + "Q " + ". "*(BOARD_SIZE-q-1) for q in queens))
---
+++ itertools.py
@@ -0,0 +1,14 @@
+from itertools import groupby
+lines = ('\n'
+         'This is the\n'
+         'first paragraph.\n'
+         '\n'
+         'This is the second.\n').splitlines()
+# Use itertools.groupby and bool to return groups of
+# consecutive lines that either have content or don't.
+for has_chars, frags in groupby(lines, bool):
+    if has_chars:
+        print(' '.join(frags))
+# PRINTS:
+# This is the first paragraph.
+# This is the second.

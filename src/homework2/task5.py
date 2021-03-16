cb4560948ae0c9edc2921bb67cb9cfc358954c6d
Anatol <79664416+seddy-evv@users.noreply.github.com>
2021-03-17 00:08:43 +0300

    Update task5.py

--- src/homework2/task5.py
+++ src/homework2/task5.py
@@ -2,7 +2,7 @@ def fibonacci(n):
     fibonacci_number = 0
     first_number = 0
     second_number = 1
-    for i in range(3, n+1):
+    for i in range(3, n + 1):
         fibonacci_number = first_number + second_number
         first_number = second_number
         second_number = fibonacci_number

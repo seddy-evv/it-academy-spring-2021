3032ba999cd588062df2560c1637547f91674d97
Anatol <seddboom@gmail.com>
2021-04-11 18:53:43 +0300

    update task1 and task5

--- src/homework2/task5.py
+++ src/homework2/task5.py
@@ -4,7 +4,7 @@
 def fibonacci(n):
     fibonacci_number = 1
     first_number = 0
-    for i in range(3, n+1):
+    for i in range(3, n + 1):
         fibonacci_number, first_number = fibonacci_number + first_number, fibonacci_number
     fibonacci_number = 1 if n == 2 else fibonacci_number
     print(fibonacci_number)

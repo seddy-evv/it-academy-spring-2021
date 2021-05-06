d1d1f51fc687b1f6427d0112a4638cc006df6786
Anatol <seddboom@gmail.com>
2021-05-06 21:59:16 +0300

    add 5 and 4 kyu tasks

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -178,3 +178,136 @@ def parts_sums(list_):
 
 
 print(parts_sums([1, 2, 3, 4, 5, 6]))
+
+
+"""A friend of mine takes the sequence of all numbers from 1 to n (where n > 0).
+Within that sequence, he chooses two numbers, a and b.
+He says that the product of a and b should be equal to the sum of all numbers in the sequence,
+excluding a and b.
+Given a number n, could you tell me the numbers he excluded from the sequence?
+The function takes the parameter: n (n is always strictly greater than 0)
+and returns an array or a string (depending on the language) of the form:
+
+[(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
+with all (a, b) which are the possible removed numbers in the sequence 1 to n.
+
+[(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ... will be sorted in increasing order
+of the "a".
+
+It happens that there are several possible (a, b). The function returns an empty array
+(or an empty string) if
+no possible numbers are found which will prove that my friend has not told the truth!
+(Go: in this case return nil).
+
+Examples:
+removNb(26) should return [(15, 21), (21, 15)]
+or
+removNb(26) should return { {15, 21}, {21, 15} }
+or
+removeNb(26) should return [[15, 21], [21, 15]]
+or
+removNb(26) should return [ {15, 21}, {21, 15} ]
+or
+removNb(26) should return "15 21, 21 15"
+or
+
+in C:
+removNb(26) should return  {{15, 21}{21, 15}} tested by way of strings.
+Function removNb should return a pointer to an allocated array of Pair pointers,
+each one also allocated."""
+
+
+def remov_nb(n):
+    total = 0
+    list_high = []
+    list_low = []
+    for i in range(n + 1):
+        total += i
+    for i in range(n, 0, - 1):
+        z = (total - i)//i
+        if i * z == (total - i - z):
+            list_low.append((z, i))
+            list_high.append((i, z))
+    list_high.reverse()
+    return list_low + list_high
+
+
+print(remov_nb(26))
+
+
+"""Given two strings s1 and s2, we want to visualize how different the two strings are. 
+We will only take into account the lowercase letters (a to z). 
+First let us count the frequency of each lowercase letters in s1 and s2.
+s1 = "A aaaa bb c"
+s2 = "& aaa bbb c d"
+s1 has 4 'a', 2 'b', 1 'c'
+s2 has 3 'a', 3 'b', 1 'c', 1 'd'
+
+So the maximum for 'a' in s1 and s2 is 4 from s1; the maximum for 'b' is 3 from s2. 
+In the following we will not consider letters when the maximum of their occurrences 
+is less than or equal to 1.
+
+We can resume the differences between s1 and s2 in the following string: "1:aaaa/2:bbb" 
+where 1 in 1:aaaa stands for string s1 and aaaa because the maximum for a is 4. 
+In the same manner 2:bbb stands for string s2 and bbb because the maximum for b is 3.
+
+The task is to produce a string in which each lowercase letters of s1 or s2 appears 
+as many times as its maximum if this maximum is strictly greater than 1; 
+these letters will be prefixed by the number of the string where they appear 
+with their maximum value and :. If the maximum is in s1 as well as in s2 the prefix is =:.
+
+In the result, substrings (a substring is for example 2:nnnnn or 1:hhh; 
+it contains the prefix) will be in decreasing order of their length and when they have 
+the same length sorted in ascending lexicographic order (letters and digits - more precisely 
+sorted by codepoint); the different groups will be separated by '/'. 
+See examples and "Example Tests".
+
+Hopefully other examples can make this clearer.
+
+s1 = "my&friend&Paul has heavy hats! &"
+s2 = "my friend John has many many friends &"
+mix(s1, s2) --> "2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+
+s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
+s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
+mix(s1, s2) --> "1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+
+s1="Are the kids at home? aaaaa fffff"
+s2="Yes they are here! aaaaa fffff"
+mix(s1, s2) --> "=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh"""""
+
+
+def mix(s1, s2):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    dict_s1 = {}
+    dict_s2 = {}
+    max_len = 0
+    for el in s1:
+        if el in letters:
+            dict_s1[el] = dict_s1.get(el, 0) + 1
+            max_len = dict_s1[el] if dict_s1[el] > max_len else max_len
+    for el in s2:
+        if el in letters:
+            dict_s2[el] = dict_s2.get(el, 0) + 1
+            max_len = dict_s2[el] if dict_s2[el] > max_len else max_len
+    list_of_letters = ["" for _ in range(max_len + 1)]
+    for el in letters:
+        sum_string1 = dict_s1.get(el, 0)
+        sum_string2 = dict_s2.get(el, 0)
+        if sum_string1 > sum_string2 and sum_string1 > 1:
+            list_of_letters[sum_string1] = list_of_letters[sum_string1] + "1:" + el * sum_string1 + "/"
+    for el in letters:
+        sum_string1 = dict_s1.get(el, 0)
+        sum_string2 = dict_s2.get(el, 0)
+        if sum_string1 < sum_string2 and sum_string2 > 1:
+            list_of_letters[sum_string2] = list_of_letters[sum_string2] + "2:" + el * sum_string2 + "/"
+    for el in letters:
+        sum_string1 = dict_s1.get(el, 0)
+        sum_string2 = dict_s2.get(el, 0)
+        if sum_string1 == sum_string2 and sum_string1 > 1:
+            list_of_letters[sum_string1] = list_of_letters[sum_string1] + "=:" + el * sum_string1 + "/"
+    list_of_letters.reverse()
+    return("".join(list_of_letters)).strip("/")
+
+
+print(mix("Sadus:cpms>orqn3zecwGvnznSgacs", "MynwdKizfd$lvse+gnbaGydxyXzayp"))

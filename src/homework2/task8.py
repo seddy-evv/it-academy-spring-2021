3b9f4cb97820cdd7ceb33dc581c820ce1e4adb21
Anatol <seddboom@gmail.com>
2021-05-06 22:08:19 +0300

    flake8 bugs fixed

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -224,7 +224,7 @@ def remov_nb(n):
     for i in range(n + 1):
         total += i
     for i in range(n, 0, - 1):
-        z = (total - i)//i
+        z = (total - i) // i
         if i * z == (total - i - z):
             list_low.append((z, i))
             list_high.append((i, z))
@@ -235,31 +235,31 @@ def remov_nb(n):
 print(remov_nb(26))
 
 
-"""Given two strings s1 and s2, we want to visualize how different the two strings are. 
-We will only take into account the lowercase letters (a to z). 
+"""Given two strings s1 and s2, we want to visualize how different the two strings are.
+We will only take into account the lowercase letters (a to z).
 First let us count the frequency of each lowercase letters in s1 and s2.
 s1 = "A aaaa bb c"
 s2 = "& aaa bbb c d"
 s1 has 4 'a', 2 'b', 1 'c'
 s2 has 3 'a', 3 'b', 1 'c', 1 'd'
 
-So the maximum for 'a' in s1 and s2 is 4 from s1; the maximum for 'b' is 3 from s2. 
-In the following we will not consider letters when the maximum of their occurrences 
+So the maximum for 'a' in s1 and s2 is 4 from s1; the maximum for 'b' is 3 from s2.
+In the following we will not consider letters when the maximum of their occurrences
 is less than or equal to 1.
 
-We can resume the differences between s1 and s2 in the following string: "1:aaaa/2:bbb" 
-where 1 in 1:aaaa stands for string s1 and aaaa because the maximum for a is 4. 
+We can resume the differences between s1 and s2 in the following string: "1:aaaa/2:bbb"
+where 1 in 1:aaaa stands for string s1 and aaaa because the maximum for a is 4.
 In the same manner 2:bbb stands for string s2 and bbb because the maximum for b is 3.
 
-The task is to produce a string in which each lowercase letters of s1 or s2 appears 
-as many times as its maximum if this maximum is strictly greater than 1; 
-these letters will be prefixed by the number of the string where they appear 
+The task is to produce a string in which each lowercase letters of s1 or s2 appears
+as many times as its maximum if this maximum is strictly greater than 1;
+these letters will be prefixed by the number of the string where they appear
 with their maximum value and :. If the maximum is in s1 as well as in s2 the prefix is =:.
 
-In the result, substrings (a substring is for example 2:nnnnn or 1:hhh; 
-it contains the prefix) will be in decreasing order of their length and when they have 
-the same length sorted in ascending lexicographic order (letters and digits - more precisely 
-sorted by codepoint); the different groups will be separated by '/'. 
+In the result, substrings (a substring is for example 2:nnnnn or 1:hhh;
+it contains the prefix) will be in decreasing order of their length and when they have
+the same length sorted in ascending lexicographic order (letters and digits - more precisely
+sorted by codepoint); the different groups will be separated by '/'.
 See examples and "Example Tests".
 
 Hopefully other examples can make this clearer.
@@ -295,17 +295,20 @@ def mix(s1, s2):
         sum_string1 = dict_s1.get(el, 0)
         sum_string2 = dict_s2.get(el, 0)
         if sum_string1 > sum_string2 and sum_string1 > 1:
-            list_of_letters[sum_string1] = list_of_letters[sum_string1] + "1:" + el * sum_string1 + "/"
+            list_of_letters[sum_string1] = list_of_letters[sum_string1] + "1:" \
+                                           + el * sum_string1 + "/"
     for el in letters:
         sum_string1 = dict_s1.get(el, 0)
         sum_string2 = dict_s2.get(el, 0)
         if sum_string1 < sum_string2 and sum_string2 > 1:
-            list_of_letters[sum_string2] = list_of_letters[sum_string2] + "2:" + el * sum_string2 + "/"
+            list_of_letters[sum_string2] = list_of_letters[sum_string2] + "2:" \
+                                           + el * sum_string2 + "/"
     for el in letters:
         sum_string1 = dict_s1.get(el, 0)
         sum_string2 = dict_s2.get(el, 0)
         if sum_string1 == sum_string2 and sum_string1 > 1:
-            list_of_letters[sum_string1] = list_of_letters[sum_string1] + "=:" + el * sum_string1 + "/"
+            list_of_letters[sum_string1] = list_of_letters[sum_string1] + "=:" \
+                                           + el * sum_string1 + "/"
     list_of_letters.reverse()
     return("".join(list_of_letters)).strip("/")
 
